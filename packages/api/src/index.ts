3402f1c1f66051f583345b2c727f4b72a3741cfc
kobayashi <823913645@qq.com>
2025-01-12 19:13:50 +0800

    feat: add api request params validator

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,9 +1,58 @@
+import Ajv, { ValidateFunction } from 'ajv'
 import { createGenerator } from 'ts-json-schema-generator'
 import * as path from 'path'
 
-const generator = createGenerator({
-  path: path.resolve(__dirname, 'types.ts'),
-  type: '*',
-})
+const ajv = new Ajv({ allErrors: true })
 
-console.dir(JSON.stringify(generator.createSchema()))
+const cachedValidator: Record<string, ValidateFunction> = {}
+
+const urlAndTypeNameMap: Record<string, string> = {
+  '/v1/call/record/updateStatus': 'Person',
+}
+
+const getValidator = (url: string) => {
+  const cached = cachedValidator[url]
+
+  if (cached) {
+    return cached
+  }
+
+  const typeName = urlAndTypeNameMap[url]
+
+  if (typeName) {
+    const generator = createGenerator({
+      path: path.resolve(__dirname, 'types.ts'),
+      type: '*',
+    })
+
+    const typeDefinitionsJSONSchema = generator.createSchema()
+
+    ajv.addSchema(typeDefinitionsJSONSchema, 'rootSchema')
+    const validator = ajv.getSchema(`rootSchema#/definitions/${typeName}`);
+    
+    if (validator) {
+      cachedValidator[url] = validator
+    }
+
+    return validator
+  }
+
+  return null
+}
+
+// use the below code in request interceptor
+const validator = getValidator('/v1/call/record/updateStatus')
+
+if (validator) {
+  const pass = validator({
+    firstName: 'Ayanami',
+    lastName: 'Rei',
+    age: 16
+  })
+
+  if (pass) {
+    console.log('validate success!!!')
+  } else {
+    console.log(validator.errors)
+  }
+}
